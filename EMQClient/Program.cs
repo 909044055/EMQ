a1d30eb99bf2279892058b77d7d8fcb8fba5399f
zwj <909044055@qq.com>
2019-11-27 16:38:31 +0800

    init

---
+++ EMQClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace EMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // create client instance 
+            MqttClient client = new MqttClient("192.168.12.107");
+
+            // register to message received 
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            string clientId = "jy001";
+            client.Connect(clientId);
+
+            // subscribe to the topic "/home/temperature" with QoS 2 
+            client.Subscribe(new string[] { "/home/111" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+
+            Console.ReadLine();
+        }
+
+
+
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // handle message received 
+            Console.WriteLine(Encoding.UTF8.GetString( e.Message));
+
+        }
+
+    }
+
+
+
+
+}
---
+++ EMQServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace EMQServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MqttClient client = new MqttClient("192.168.12.107");
+
+            string clientId = "jy002";
+            client.Connect(clientId);
+
+
+            var i = 0;
+
+
+            while (true)
+            {
+                i++;
+                Thread.Sleep(1000);
+
+
+                Console.WriteLine($"PubTopic Start {i}");
+
+                // publish a message on "/home/temperature" topic with QoS 2 
+                client.Publish("/home/111", Encoding.UTF8.GetBytes(i.ToString()), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+
+            }
+
+        }
+    }
+}
